7040846c0cb944b0414e302959d25060af8d4627
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-01-28 01:44:27 +0600

    Changing the root

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,15 @@
-import { registerRootComponent } from "expo";
-import App from "@/App";
+import React from "react";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "../cache"; 
+import RootNavigator from "../navigation/RootNavigator";
 
-registerRootComponent(App);
-export default App;
+export default function App() {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY} // Replace with your Clerk Frontend API key
+      tokenCache={tokenCache}
+    >
+      <RootNavigator />
+    </ClerkProvider>
+  );
+}
